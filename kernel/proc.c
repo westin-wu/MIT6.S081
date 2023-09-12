c306ff57a438f9292139c0d0c924128434f4e83b
westin <1602775103@qq.com>
2023-09-12 21:14:06 +0800

    syscall实验

--- kernel/proc.c
+++ kernel/proc.c
@@ -21,6 +21,30 @@ static void freeproc(struct proc *p);
 
 extern char trampoline[]; // trampoline.S
 
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
+}
+
 // initialize the proc table at boot time.
 void
 procinit(void)
